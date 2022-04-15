e59ec9fdabbbfb0d745c7ac0dae11bc995ed4045
Christopher Lefevre <clefevre@transparent.com>
2022-04-15 12:09:54 -0400

    Initial commit, basics of Spring Initializr with libraries required.

---
+++ src/test/java/com/clefevre/rest/fun/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clefevre.rest.fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
